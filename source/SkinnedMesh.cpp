b6220fc7cc2ede4f3a88dee636052ccfa50d64f6
vonLeebpl <vonleebpl@gmail.com>
2020-03-26 09:31:29 +0100

    Added WK classes

--- source/SkinnedMesh.cpp
+++ source/SkinnedMesh.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
+#include "MeshBuffer.h"
 #include "AnimatedMesh.h"
 #include "SkinnedMesh.h"
+#include "SJoint.h"
 
 using namespace irr;
 using namespace System;
@@ -39,6 +41,38 @@ void SkinnedMesh::FinalizeMeshPopulation()
 	m_SkinnedMesh->finalize();
 }
 
+void SkinnedMesh::AddMeshBuffer(MeshBuffer^ meshBuffer)
+{
+	LIME_ASSERT(meshBuffer != nullptr);
+
+	SSkinMeshBuffer* buffer = m_SkinnedMesh->addMeshBuffer();
+	buffer->Vertices_Standard.reallocate(meshBuffer->VertexCount);
+	for (int i=0; i < meshBuffer->VertexCount; i++)
+	{
+		buffer->Vertices_Standard.push_back(video::S3DVertex());
+		buffer->Vertices_Standard[i].Pos = (irr::core::vector3df)meshBuffer->GetVertex(i)->Position;
+		buffer->Vertices_Standard[i].Color = (irr::video::SColor)meshBuffer->GetVertex(i)->Color;
+		buffer->Vertices_Standard[i].TCoords = (core::vector2df)meshBuffer->GetVertex(i)->TCoords;
+	}
+	buffer->Indices.set_used(meshBuffer->GetIndices16Bit()->Count);
+	for (int i = 0; i < meshBuffer->GetIndices16Bit()->Count; i++)
+	{
+		buffer->Indices[i] = meshBuffer->GetIndices16Bit()[i];
+	}
+}
+
+SJoint^ SkinnedMesh::AddJoint()
+{
+	SJoint^ newJoint = gcnew SJoint(m_SkinnedMesh->addJoint());
+	Joints->Add(newJoint);
+	return newJoint;
+}
+
+List<SJoint^>^ SkinnedMesh::GetAllJoints()
+{
+	return Joints;
+}
+
 String^ SkinnedMesh::GetJointName(int index)
 {
 	LIME_ASSERT(index >= 0 && index < JointCount);
@@ -49,6 +83,26 @@ int SkinnedMesh::GetJointIndex(String^ name)
 {
 	LIME_ASSERT(name != nullptr);
 	return m_SkinnedMesh->getJointNumber(LIME_SAFESTRINGTOSTRINGC_C_STR(name));
+}
+
+SRotationKey^ SkinnedMesh::AddRotationKey(SJoint^ joint)
+{
+	return gcnew SRotationKey(m_SkinnedMesh->addRotationKey(joint->m_Joint));
+}
+
+SPositionKey^ SkinnedMesh::AddPositionKey(SJoint^ joint)
+{
+	return gcnew SPositionKey(m_SkinnedMesh->addPositionKey(joint->m_Joint));
+}
+
+SScaleKey^ SkinnedMesh::AddScaleKey(SJoint^ joint)
+{
+	return gcnew SScaleKey(m_SkinnedMesh->addScaleKey(joint->m_Joint));
+}
+
+SWeight^ SkinnedMesh::AddWeight(SJoint^ joint)
+{
+	return gcnew SWeight(m_SkinnedMesh->addWeight(joint->m_Joint));
 }
 
 bool SkinnedMesh::SetHardwareSkinning(bool turnOn)
--- source/SkinnedMesh.h
+++ source/SkinnedMesh.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include "stdafx.h"
+#include "MeshBuffer.h"
 #include "AnimatedMesh.h"
+#include "SJoint.h"
+#include "SRotationKey.h"
+#include "SPositionKey.h"
+#include "SScaleKey.h"
+#include "SWeight.h"
 
 using namespace irr;
 using namespace System;
@@ -10,6 +16,8 @@ using namespace IrrlichtLime::Core;
 namespace IrrlichtLime {
 namespace Scene {
 
+ref class MeshBuffer;
+
 public ref class SkinnedMesh : AnimatedMesh
 {
 public:
@@ -17,9 +25,17 @@ public:
 	void AnimateMesh(float frame, float blend);
 	void ConvertMeshToTangents();
 	void FinalizeMeshPopulation();
+	void AddMeshBuffer(MeshBuffer^ meshBuffer);
 
+	SJoint^ AddJoint();
+	List<SJoint^>^ GetAllJoints();
 	String^ GetJointName(int index);
 	int GetJointIndex(String^ name);
+
+	SRotationKey^ AddRotationKey(SJoint^ joint);
+	SPositionKey^ AddPositionKey(SJoint^ joint);
+	SScaleKey^ AddScaleKey(SJoint^ joint);
+	SWeight^ AddWeight(SJoint^ joint);
 
 	bool SetHardwareSkinning(bool turnOn);
 	void SetInterpolationMode(InterpolationMode mode);
@@ -32,6 +48,10 @@ public:
 
 	property int JointCount { int get(); }
 	property bool Static { bool get(); }
+
+private:
+
+	List<SJoint^>^ Joints = gcnew List<SJoint^>();
 
 internal:
 
